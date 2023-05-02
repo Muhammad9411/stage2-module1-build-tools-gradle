42be609f9f31579c39662dda8fe9f8a9c217bf70
MuhammadJon <mtjohn9411@gmail.com>
2023-05-02 15:12:36 +0500

    Changes

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -5,8 +5,8 @@ public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         boolean result = false;
         try {
-            double isPositive = Double.parseDouble(str);
             if (str != null && !str.equals("")) {
+                double isPositive = Double.parseDouble(str);
                 if (isPositive > 0) {
                     result = true;
                 }
